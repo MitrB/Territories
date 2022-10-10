84a5b93c83c856dfb1be0dbae8c4b1acbe63b56a
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-10-10 13:34:11 +0200

    test

--- gui.py
+++ gui.py
@@ -67,7 +67,6 @@ def main():
     # N = randint(10, 500)
     N = 650
     points = generate_points(N)
-    print(points)
     info(points)
     hull = convex(points)
     triangles = calculate_pbp_triangulation(points)
--- tests.py
+++ tests.py
@@ -3,11 +3,26 @@ from helper import *
 
 class TestMakeNRandomPoints(unittest.TestCase):
 
-    def test_amount_of_points(self):
-        N = 10
-        points = generate_points(N)
-        self.assertEqual(len(points), N)
+    def __init__(self, *args, **kwargs):
+        super(TestMakeNRandomPoints, self).__init__(*args, **kwargs)
+        self.N_1 = 10
+        self.N_2 = 100
+        self.N_3 = 0 
+        self.points_1 = generate_points(self.N_1)
+        self.points_2 = generate_points(self.N_2)
+        self.points_3 = generate_points(self.N_3)
 
+
+    def test_amount_of_points(self):
+        self.assertEqual(len(self.points_1), self.N_1)
+        self.assertEqual(len(self.points_2), self.N_2)
+        self.assertEqual(len(self.points_3), self.N_3)
+
+    def test_correct_format(self):
+        for point in self.points_1:
+            self.assertEqual(type((1,2)), type(point))
+            self.assertEqual(type(1), type(point[0]))
+            self.assertEqual(type(1), type(point[1]))
 
 if __name__ == '__main__':
     unittest.main()
