51a517efd3cc5f51c65f548d93080bffd5fe03e4
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-10-21 01:40:50 +0200

    line_counter

--- gui.py
+++ gui.py
@@ -2,11 +2,13 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import Tk, Canvas, Frame, BOTH
 from logging import debug, info
-from random import randint,choice
+from random import randint, choice
 from math_functions import *
 from helper import *
 from GLOBVAR import *
 import logging
+
+from line_profiler import LineProfiler
 
 logging.basicConfig(level=logging.INFO, filename="debug.log", filemode="w")
 
@@ -47,12 +49,13 @@ class Display(Frame):
     def drawTriangle(self, triangle):
         p1, p2, p3 = triangle[0], triangle[1], triangle[2]
         color = "#"+''.join([choice('0123456789ABCDEF') for j in range(6)])
-        self.canvas.create_polygon(p1[0],p1[1],p2[0],p2[1],p3[0],p3[1], fill=color)
+        self.canvas.create_polygon(
+            p1[0], p1[1], p2[0], p2[1], p3[0], p3[1], fill=color)
 
     def drawTriangleOutline(self, triangle):
         p1, p2, p3 = triangle[0], triangle[1], triangle[2]
         self.canvas.create_line(p1[0], p1[1], p2[0],
-                               p2[1], p3[0], p3[1], p1[0], p1[1], fill="black")
+                                p2[1], p3[0], p3[1], p1[0], p1[1], fill="black")
 
     def add_label(self, points):
         for point in points:
@@ -62,8 +65,7 @@ class Display(Frame):
 
 def main():
 
-
-    # calculate 
+    # calculate
     N = 1000
     points = generate_points(N)
     info(points)
@@ -71,7 +73,6 @@ def main():
     info(triangles)
     triangles = calculate_delaunay(triangles)
     info(triangles)
-
 
     # Tk
     root = Tk()
@@ -84,4 +85,11 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    lp = LineProfiler()
+    lp.add_function(calculate_delaunay)
+    lp.add_function(calculate_triangle_pairs)
+    lp.add_function(find_common_edge)
+    lp.add_function(make_locally_delaunay)
+    lp_wrapper = lp(main)
+    lp_wrapper()
+    lp.print_stats()
