d3d42a7c25165cecf6365591531fff1028f682a6
Dmitry Buggenhout <dmitry.buggenhout@gmai.com>
2022-10-21 19:42:35 +0200

    territory

---
+++ territory.py
@@ -0,0 +1,19 @@
+class Map():
+    """Collection of Polygons"""
+    # pairs are polygons that share an edge: {edge: p1, p2}
+    def __init__(self, pairs):
+        polygons = []
+
+    def coord_to_Polygon(self):
+        pass
+
+class Polygon():
+    """Mathematical presentation of a region"""
+
+    def __init__(self):
+        neighbours = []
+
+
+class Territory(Polygon):
+    """A Territory is a Polygon with properties of the land it represents"""
+    pass
