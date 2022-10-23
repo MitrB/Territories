3d6b4cc4d3e638c422f04f3f758f252b46db9c59
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-10-23 17:58:20 +0200

    profiler poetry

--- dev/profiler/profiler.py
+++ dev/profiler/profiler.py
@@ -2,11 +2,13 @@ from line_profiler import LineProfiler
 from src.math_functions.math_functions import *
 from src.gui.gui import main
 
-lp = LineProfiler(main)
-lp.add_function(calculate_delaunay)
-lp.add_function(calculate_triangle_pairs)
-lp.add_function(find_common_edge)
-lp.add_function(make_locally_delaunay)
-lp_wrapper = lp(main)
-lp_wrapper()
-lp.print_stats()
+
+def profile():
+    lp = LineProfiler(main)
+    lp.add_function(calculate_delaunay)
+    lp.add_function(calculate_triangle_pairs)
+    lp.add_function(find_common_edge)
+    lp.add_function(make_locally_delaunay)
+    lp_wrapper = lp(main)
+    lp_wrapper()
+    lp.print_stats()
